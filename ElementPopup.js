4e324764cc7972413c66438fda9fd602609a9099
Roelf47 <roelfellis@gmail.com>
2025-03-08 18:22:38 +0200

    ElementPopup - set cancel button default position

--- ElementPopup.js
+++ ElementPopup.js
@@ -41,6 +41,16 @@ export class ElementPopup{
     static #template = document.createElement("template");
     static #template_innerHTML = this.#template.innerHTML = /*HTML*/ `
         <div class="tPopup">
+            <style>
+                .tElementContainer{
+                    position: relative;
+                }
+                .tClosePopup{
+                    position: absolute;
+                    top: 0;
+                    right: 1em;
+                }
+            </style>
             <div class="tElementContainer">
                 <div class="tClosePopup">x</div>
                 <div class="tPopupContainer></div>
