fd19b7fd8664885b45a2a932e9aba0a0886172e0
Roelf47 <roelfellis@gmail.com>
2025-03-09 12:20:20 +0200

    ManageImagesPopup - added

---
+++ ManageImagesPopup.js
@@ -0,0 +1,59 @@
+export class ManageImagesPopup{
+    /**
+     * 
+     * @param {Object} obj 
+     * @param {HTMLElement} obj.parent
+     * @param {Object[]} obj.images
+     * @param {String} obj[].images.url
+     * @param {String} obj[].images.name
+     */
+    constructor(obj){
+        /** @type {DocumentFragment} */
+        const df = ManageImagesPopup.#template.content.cloneNode(true);
+
+        const popup = df.querySelector(".t-popup");
+
+        if(obj.parent){
+            obj.parent.append(df);
+        }else{
+            document.body.append(df);
+        }
+
+        const manageImageContainer = popup.querySelector(".t-manage-image-container");
+
+        for(const imageData of obj.images){
+            /** @type {DocumentFragment} */
+            const imageDf = ManageImagesPopup.#imageTemplate.content.cloneNode(true);
+
+            const imageContainer = imageDf.querySelector(".t-image-container");
+            manageImageContainer.append(imageContainer);
+
+            imageContainer.querySelector("img").src = imageData["url"];
+            imageContainer.querySelector(".t-image-name").innerText = imageData["name"];
+        }
+
+        
+    }
+
+    static #template = document.createElement("template");
+    static #template_innerHTML = this.#template.innerHTML = /*HTML*/`
+        <div class="t-popup">
+            <div class="t-manage-image-container">
+                <div class="t-images-container"></div>
+                <div class="t-button-container">
+                    <button class="t-select-button">Select</button>
+                    <button class="t-close-button">Close</button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    static #imageTemplate = document.createElement("template");
+    static #imageTemplate_innerHTML = this.#imageTemplate.innerHTML = /*HTML*/`
+        <div class="t-image-container">
+            <img>
+            <p class="t-image-name"></p>
+            <button class="t-delete-button">Delete</button>
+        </div>
+    `;
+}
